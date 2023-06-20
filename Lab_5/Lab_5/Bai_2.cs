6ae2d3562efaa7a5d14ba7764710ffa4e8f3ae27
hgbao226 <hgbao226@gmail.com>
2023-06-20 22:31:22 +0700

    done lab 5 form 2

--- Lab_5/Lab_5/Bai_2.cs
+++ Lab_5/Lab_5/Bai_2.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Net.Mail;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
-using System;
-using System.Windows.Forms;
-using MailKit.Net.Imap;
+﻿using MailKit.Net.Imap;
 using MailKit.Search;
 using MailKit;
 using MimeKit;
@@ -40,24 +27,28 @@ namespace Lab_5
             {
                 try
                 {
-                    client.Connect("nhom5.nt106", 993, SecureSocketOptions.SslOnConnect);
+                    client.Connect("127.0.0.1", 143, SecureSocketOptions.None);
                     client.Authenticate(email, password);
                     var inbox = client.Inbox;
                     inbox.Open(FolderAccess.ReadOnly);
                     listView1.Items.Clear();
+                    listView1.Columns.Add("Email", 200);
+                    listView1.Columns.Add("From", 100);
+                    listView1.Columns.Add("Thời gian", 100);
+                    listView1.View = View.Details;
                     for (int i = 0; i < inbox.Count; i++)
                     {
                         var message = inbox.GetMessage(i);
-                        var name = new ListViewItem(message.Subject);
-
-                        var from = new ListViewItem.ListViewSubItem(name, message.From.ToString());
+                        ListViewItem name = new ListViewItem(message.Subject);
+                        ListViewItem.ListViewSubItem from = new
+                        ListViewItem.ListViewSubItem(name, message.From.ToString());
                         name.SubItems.Add(from);
-
-                        var date = new ListViewItem.ListViewSubItem(name, message.Date.Date.ToString());
+                        ListViewItem.ListViewSubItem date = new
+                        ListViewItem.ListViewSubItem(name, message.Date.Date.ToString());
                         name.SubItems.Add(date);
-
                         listView1.Items.Add(name);
                     }
+
                     client.Disconnect(true);
                     MessageBox.Show("Emails retrieved successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
@@ -65,6 +56,7 @@ namespace Lab_5
                 {
                     MessageBox.Show("An error occurred while retrieving emails: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
+
             }
         }
     }
